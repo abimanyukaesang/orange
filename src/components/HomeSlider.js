823da54f9bb84f6f4b17cd029edc9375650903a2
tree bagas <tree.bagas@gmail.com>
2020-03-31 22:03:10 +0700

    update modal

--- src/components/HomeSlider.js
+++ src/components/HomeSlider.js
@@ -26,10 +26,28 @@ const HomeSlider =() =>(
                             <div className="col-md-8">
                                 <h2>NEVER STOP EXPLORING</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <a href='#'>
+                                <a href='#' data-toggle="modal" data-target="#exampleModal">
                                     WATCH VIDEO
                                     <i className="fa fa-play-circle" aria-hidden="true"></i>
                                 </a>
+
+                                
+                                <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div className="modal-dialog" role="document">
+                                        <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div className="modal-body">
+                                            ...makan
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
                     </div>
