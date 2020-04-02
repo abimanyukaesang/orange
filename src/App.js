c883a2f8d5ea4b7cd180e48eb2968c6dedb5243c
tree bagas <tree.bagas@gmail.com>
2020-04-02 19:07:19 +0700

    update header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
 
 import {BrowserRouter as Router, Route, Link, NavLink, Switch} from 'react-router-dom';
 import Home from './pages/Home';
@@ -39,8 +37,12 @@ function App() {
                       </li>
                   </ul>
               </div>
+              
             </div>
 
+            <div className='container'>
+              <div className='line'></div>
+            </div>
         </nav>
 
  
