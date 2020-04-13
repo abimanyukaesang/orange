5318d87a2ef9b4e4cb8ce38e126954998819139a
tree bagas <tree.bagas@gmail.com>
2020-04-13 16:44:52 +0700

    update

--- src/components/ServiceContent.js
+++ src/components/ServiceContent.js
@@ -7,27 +7,29 @@ const ServiceContent = (data) => {
   const items = data.items;
   if (items.position === "left") {
     return (
-      <div className="row p-0 m-0">
-        <div className="col-md-6 order-sm-12 p-0">
-          <img src={items.image} alt={items.title} className="img-fluid" />
-        </div>
-        <div className="col-md-6 order-sm-1 p-0">
-          <div className="wrp-content-service">
-            <img src={bgServiceBlank} alt={items.title} className="img-fluid" />
-            <div className="content-service flex-row-reverse">
-              <div className="item-service pdr">
-                <img src={items.icon} alt={items.title} />
+      <div className={items.className}>
+        <div className="row p-0 m-0">
+          <div className="col-md-6 order-sm-12 p-0">
+            <img src={items.image} alt={items.title} className="img-fluid" />
+          </div>
+          <div className="col-md-6 order-sm-1 p-0">
+            <div className="wrp-content-service">
+              <img src={bgServiceBlank} alt={items.title} className="img-fluid" />
+              <div className="content-service flex-row-reverse">
+                <div className="item-service pdr">
+                  <img src={items.icon} alt={items.title} />
 
-                <h2>{items.title}</h2>
-                <p>{items.desc}</p>
-                <Link
-                  to={{
-                    pathname: `/services/${items.slug}`
-                  }}
-                  className="btn btn-orange"
-                >
-                  LEARN MORE
-                </Link>
+                  <h2>{items.title}</h2>
+                  <p>{items.desc}</p>
+                  <Link
+                    to={{
+                      pathname: `/services/${items.slug}`
+                    }}
+                    className="btn btn-orange"
+                  >
+                    LEARN MORE
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
@@ -37,28 +39,30 @@ const ServiceContent = (data) => {
   }
   if (items.position === "right") {
     return (
-      <div className="row p-0 m-0">
-        <div className="col-md-6 order-sm-1 p-0">
-          <img src={items.image} alt="" className="img-fluid" />
-        </div>
-        <div className="col-md-6 order-sm-12 p-0">
-          <div className="wrp-content-service">
-            <img src={bgServiceBlank} alt="" className="img-fluid" />
-            <div className="content-service bg-grey">
-              <div className="item-service pdl">
-                <img src={items.icon} alt={items.title} />
+      <div className={items.className}>
+        <div className="row p-0 m-0">
+          <div className="col-md-6 order-sm-1 p-0">
+            <img src={items.image} alt="" className="img-fluid" />
+          </div>
+          <div className="col-md-6 order-sm-12 p-0">
+            <div className="wrp-content-service">
+              <img src={bgServiceBlank} alt="" className="img-fluid" />
+              <div className="content-service bg-grey">
+                <div className="item-service pdl">
+                  <img src={items.icon} alt={items.title} />
 
-                <h2>{items.title}</h2>
-                <p>{items.desc}</p>
+                  <h2>{items.title}</h2>
+                  <p>{items.desc}</p>
 
-                <Link
-                  to={{
-                    pathname: `/services/${items.slug}`
-                  }}
-                  className="btn btn-orange"
-                >
-                  LEARN MORE
-                </Link>
+                  <Link
+                    to={{
+                      pathname: `/services/${items.slug}`
+                    }}
+                    className="btn btn-orange"
+                  >
+                    LEARN MORE
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
