2f010fca796b835e42d506fcd2290441f66a3750
tree bagas <tree.bagas@gmail.com>
2020-04-13 12:09:14 +0700

    revisi

--- src/components/HomeSecondSlider.js
+++ src/components/HomeSecondSlider.js
@@ -14,11 +14,23 @@ import OurService3 from '../assets/images/our-service2.jpg';
 import OurService4 from '../assets/images/our-service4.jpg';
 import OurService5 from '../assets/images/our-service5.jpg';
 
+import OurServiceControl1 from '../assets/images/our-service-control1.jpg';
+import OurServiceControl2 from '../assets/images/our-service-control2.jpg';
+import OurServiceControl3 from '../assets/images/our-service-control2.jpg';
+import OurServiceControl4 from '../assets/images/our-service-control3.jpg';
+import OurServiceControl5 from '../assets/images/our-service-control4.jpg';
+
 const contentService1 = {backgroundImage: 'url(' + OurService1 + ')',};
 const contentService2 = {backgroundImage: 'url(' + OurService2 + ')',};
 const contentService3 = {backgroundImage: 'url(' + OurService3 + ')',};
 const contentService4 = {backgroundImage: 'url(' + OurService4 + ')',};
 const contentService5 = {backgroundImage: 'url(' + OurService5 + ')',};
+
+const controlService1 = {backgroundImage: 'url(' + OurServiceControl1 + ')',};
+const controlService2 = {backgroundImage: 'url(' + OurService2 + ')',};
+const controlService3 = {backgroundImage: 'url(' + OurService3 + ')',};
+const controlService4 = {backgroundImage: 'url(' + OurService4 + ')',};
+const controlService5 = {backgroundImage: 'url(' + OurService5 + ')',};
 
 
 
@@ -39,7 +51,7 @@ const HomeSecondSlider =() =>(
                             <div className="col-md-4">
                                 <h2>Tailor Made Trips</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <Link to={`/services`} className='btn btn-red'>Learn More</Link>
+                                <Link to={`/services`} className='btn btn-gradient-orange'>Learn More</Link>
                             </div>
                         </div>
                     </div>
@@ -51,7 +63,7 @@ const HomeSecondSlider =() =>(
                             <div className="col-md-5">
                                 <h2>Corporate Event, Gathering & Meeting</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <Link to={`/services`} className='btn btn-red'>Learn More</Link>
+                                <Link to={`/services`} className='btn btn-gradient-orange'>Learn More</Link>
                             </div>
                         </div>
                     </div>
@@ -63,7 +75,7 @@ const HomeSecondSlider =() =>(
                             <div className="col-md-5">
                                 <h2>Corporate Video</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <Link to={`/services`} className='btn btn-red'>Learn More</Link>
+                                <Link to={`/services`} className='btn btn-gradient-orange'>Learn More</Link>
                             </div>
                         </div>
                     </div>
@@ -75,7 +87,7 @@ const HomeSecondSlider =() =>(
                             <div className="col-md-4">
                                 <h2>Corporate Team Building</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <Link to={`/services`} className='btn btn-red'>Learn More</Link>
+                                <Link to={`/services`} className='btn btn-gradient-orange'>Learn More</Link>
                             </div>
                         </div>
                     </div>
@@ -87,7 +99,7 @@ const HomeSecondSlider =() =>(
                             <div className="col-md-5">
                                 <h2>Themed Gala Dinner</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                <Link to={`/services`} className='btn btn-red'>Learn More</Link>
+                                <Link to={`/services`} className='btn btn-gradient-orange'>Learn More</Link>
                             </div>
                         </div>
                     </div>
@@ -105,14 +117,14 @@ const HomeSecondSlider =() =>(
                 <div className='row justify-content-end'>
                     <div className='col-md-8'>
                         <ol className="carousel-indicators">
-                            <li data-target="#HomeSecondSlider" data-slide-to="0" className="active">
+                            <li data-target="#HomeSecondSlider" data-slide-to="0" className="active" style={controlService1}>
                                 <img src={Service1} alt=''/>
                                 <p>
                                     TAILOR <br/>
                                     MADE TRIPS
                                 </p>
                             </li>
-                            <li data-target="#HomeSecondSlider" data-slide-to="1">
+                            <li data-target="#HomeSecondSlider" data-slide-to="1" style={controlService2}>
                                 <img src={Service2} alt=''/>
                                 <p>
                                     CORPORATE <br/>
@@ -120,21 +132,21 @@ const HomeSecondSlider =() =>(
                                     & MEETING
                                 </p>
                             </li>
-                            <li data-target="#HomeSecondSlider" data-slide-to="2">
+                            <li data-target="#HomeSecondSlider" data-slide-to="2" style={controlService3}>
                                 <img src={Service3} alt=''/>
                                 <p>
                                     CORPORATE <br/>
                                     VIDEO
                                 </p>
                             </li>
-                            <li data-target="#HomeSecondSlider" data-slide-to="3">
+                            <li data-target="#HomeSecondSlider" data-slide-to="3" style={controlService4}>
                                 <img src={Service4} alt=''/>
                                 <p>
                                     CORPORATE <br/>
                                     TEAM BUILDING
                                 </p>
                             </li>
-                            <li data-target="#HomeSecondSlider" data-slide-to="4">
+                            <li data-target="#HomeSecondSlider" data-slide-to="4" style={controlService5}>
                                 <img src={Service5} alt=''/>
                                 <p>
                                     THEMED<br/>
