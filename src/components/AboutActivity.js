d6844df1336efbb015f3b8dbd3feb3804b031224
tree bagas <tree.bagas@gmail.com>
2020-04-22 16:00:10 +0700

    owl react

--- src/components/AboutActivity.js
+++ src/components/AboutActivity.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/scss/AboutActivity.scss';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 import slide1 from '../assets/images/activity1.jpg';
 import slide2 from '../assets/images/activity2.jpg';
@@ -11,38 +14,64 @@ import slide7 from '../assets/images/activity7.jpg';
 import slide8 from '../assets/images/activity8.jpg';
 import slide9 from '../assets/images/activity9.jpg';
 
-const AboutActivity =() =>(
-    <div className='slide-about'>
-        <div id="about-us" className="owl-carousel">
-            <div className="about-item">
-                <img src={slide1} alt="activity"/>
+const itemOwl = [
+    { id:1, imageUrl: slide1},
+    { id:2, imageUrl: slide2},
+    { id:3, imageUrl: slide3},
+    { id:4, imageUrl: slide4},
+    { id:5, imageUrl: slide5},
+    { id:6, imageUrl: slide6},
+    { id:7, imageUrl: slide7},
+    { id:8, imageUrl: slide8},
+    { id:9, imageUrl: slide9}
+]
+
+class AboutActivity extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            responsive:{
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 1,
+                },
+                1000: {
+                    items: 2,
+                },
+            },
+        };
+    }
+
+    render() {
+        return (
+            <div className='slide-about'>
+                <OwlCarousel
+                    id="about-us"
+                    className="owl-theme"
+                    center={true}
+                    loop
+                    margin={10}
+                    nav 
+                    dots    ={false}
+                    items={5}
+                    autoplay
+                    responsive={this.state.responsive}
+                    
+                >
+                    {
+                        itemOwl.map(item => 
+                            <div className='about-item' key={item.id}>
+                                <img id={item.id} src={item.imageUrl}/>
+                            </div>
+                        )
+                    }
+                </OwlCarousel>
             </div>
-            <div className="about-item">
-                <img src={slide2} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide3} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide4} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide5} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide6} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide7} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide8} alt="activity"/>
-            </div>
-            <div className="about-item">
-                <img src={slide9} alt="activity"/>
-            </div>
-        </div>
-    </div>
-)
+        )
+    }
+}
 
 export default AboutActivity;
